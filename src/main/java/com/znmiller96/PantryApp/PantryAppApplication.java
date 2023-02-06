c59f9e0df0506c735b3b8d3118c01a1b2f3c32c3
Kangaroozac1 <znmiller96@gmail.com>
2023-02-06 13:24:32 -0600

    Initialize springboot application

---
+++ src/main/java/com/znmiller96/PantryApp/PantryAppApplication.java
@@ -0,0 +1,13 @@
+package com.znmiller96.PantryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/znmiller96/PantryApp/PantryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.znmiller96.PantryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
